d7d3051a8df41a45f608c327f2394ca8e40d7a68
M Williams <markjw@truro-penwith.ac.uk>
2020-06-05 08:37:12 +0000

    Added the chnage_to_lower function

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 def change_to_upper(text):
   return text.upper()
 
+def change_to_lower(text):
+  return text.lower()
+
 name = "Bill"
 
 print(f"Hello {change_to_upper(name)} it is good to see you.")
+print(f"Hello {change_to_lower(name)} it is good to see you.")
